47efd975ec5f9ec16c205483c0c60160e4515b8d
Carlos Monserrat Relaño <carlosmr00@gmail.com>
2024-04-18 09:49:25 +0200

    Primera subida

---
+++ src/app/maps/pages/zoom-range-page/zoomrange-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './zoomrange-page.component.html',
+  styleUrl: './zoomrange-page.component.css'
+})
+export class ZoomRangePageComponent {
+
+}
